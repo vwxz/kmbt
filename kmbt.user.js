5eabdfc14813807251bbc7c0b6600ac63576fb47
vwxz <30652779+vwxz@users.noreply.github.com>
2017-08-02 11:22:36 +0200

    Create kmbt.user.js

---
+++ kmbt.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name        kmbt
+// @namespace   http://github.com/dimotsai/vwxz/
+// @include     https://www.kurnik.pl/kalambury/*
+// @version     1
+// @description kmb bot
+// @author      vwxz
+// @grant       none
+// @run-at      document-start
+// ==/UserScript==
+
+
+(function() {
+    'use strict';
+    
+})();
